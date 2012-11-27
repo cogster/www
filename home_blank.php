b6db2895875ee13bea833792f823cde1499006ff
Rob <ssencogleader@yahoo.com>
2012-11-27 10:14:05 -0800

    Test www files

---
+++ home_blank.php
@@ -0,0 +1,59 @@
+<html> 
+
+
+<head> 
+<link
+ type="text/css" href="myotherstyle.css" />
+</head> 
+
+<body style="margin:50px 0px; padding:0px;
+	text-align:center;">
+<div style="width:1000px;
+	margin:0px auto;
+	text-align:left;
+	padding:15px;
+	">
+
+
+
+
+<div id="header" style="background-color:blue;color:white;">
+<h1 style="margin-bottom:0;">Php Test page</h1>
+</div>
+<div id="nav" style="background-color:#FFA500;">
+
+<a href="home.php">Home</a>
+<a href="home.php"> CSS</a>
+<a href="home.php"> JavaScript</a>
+<a href="home.php"> jQuery</a>
+<a href="home.php"> Ajax</a>
+<a href="home.php"> Perl</a>
+<a href="home.php"> Java</a>
+<a href="home.php"> C/C++</a>
+
+</div>
+ 
+
+
+<div id="menu" style="background-color:#FFD700;height:200px;width:100px;float:left;">
+<b>Home</b><br>
+Intro<br>
+Basics<br>
+</div>
+
+<div id="content" style="background-color:#EEEEEE;height:200px;width:900px;float:left;">
+Content goes here
+</div>
+
+<div id="footer" style="background-color:Blue;clear:both;text-align:center;color:white;">
+Copyright  W3Schools.com
+</div>
+
+</div>
+
+
+
+
+</body> 
+
+</html> 
